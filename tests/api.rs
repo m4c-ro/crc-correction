d7d4ae96924d3c533d20b51780b4c0dc9c6573c8
Cameron Duff <cameron@m4c.ro>
2025-07-22 20:00:54 +1000

    Ensure CrcCorrector, Error and Correction are Send + Sync

--- tests/api.rs
+++ tests/api.rs
@@ -1,4 +1,4 @@
-use crc_correction::{CrcCorrector, Error};
+use crc_correction::{Correction, CrcCorrector, Error};
 
 use crc::{CRC_16_ARC, CRC_32_AIXM, CRC_64_ECMA_182, Crc, Table};
 
@@ -35,4 +35,36 @@ fn too_long_fails() {
     let mut msg = [0u8; 33];
     let result = TEST_COR_64.correct(&mut msg, 0u64);
     assert_eq!(result, Err(Error::DataTooLong));
+}
+
+fn test_send_sync<T: Sync + Sync>(_: T) {}
+
+#[test]
+fn corrector_is_send_sync() {
+    test_send_sync(TEST_COR_16);
+}
+
+#[test]
+fn correction_is_send_sync() {
+    let mut msg = [0u8; 8];
+    let crc = Crc::<u16, Table<1>>::new(&CRC_16_ARC);
+    let crc = crc.checksum(&msg);
+
+    msg[1] ^= 1 << 1;
+
+    let result = TEST_COR_16.correct(&mut msg, crc);
+    assert_eq!(result, Ok(Correction::Data { error_bit: 9 }));
+
+    let c = result.unwrap();
+    test_send_sync(c);
+}
+
+#[test]
+fn error_is_send_sync() {
+    let mut msg = [0u8; 8];
+    let result = TEST_COR_16.correct(&mut msg, 0u16);
+    assert_eq!(result, Err(Error::NoError));
+
+    let e = result.unwrap_err();
+    test_send_sync(e);
 }
